68484f5d3939e6c3b2c28ce23a1b1cdfa9afc8f7
rdr <rdr.abhishek@gmail.com>
2026-09-06 22:03:07 +0530

    update cors for new vercel domain manga-ta

--- backend/app/main.py
+++ backend/app/main.py
@@ -72,6 +72,7 @@ app.add_middleware(
         "http://localhost:3001",
         "http://127.0.0.1:3000",
         "https://mangaverse-tau-umber.vercel.app",
+        "https://manga-ta.vercel.app",
         "http://127.0.0.1:3001"
     ],
     allow_credentials=True,
